56afcbb24d6f92e42ae66548d085f6190d34df52
Keerthana Kuppuchamy (SX/EAB1) <feu1cob@bosch.com>
2023-01-10 17:52:25 +0530

    Code change

--- src/test/java/TypewiseAlert/TypewiseAlertTest.java
+++ src/test/java/TypewiseAlert/TypewiseAlertTest.java
@@ -8,25 +8,25 @@ public class TypewiseAlertTest {
 
 	@Test
 	public void testCheckAndAlert() {
+		// assert alert for too high temperature - Email
 		BatteryCharacter batteryCharacter = new BatteryCharacter();
 		batteryCharacter.setCoolingType(ECoolingType.HI_ACTIVE_COOLING);
 		EAlertTarget alertTarget = EAlertTarget.TO_EMAIL;
 		assertTrue(TypewiseAlert.checkAndAlert(alertTarget, batteryCharacter, 60));
 
+		// assert alert for too low temperature - Email
+		batteryCharacter = new BatteryCharacter();
+		batteryCharacter.setCoolingType(ECoolingType.MED_ACTIVE_COOLING);
+		assertTrue(TypewiseAlert.checkAndAlert(alertTarget, batteryCharacter, -1));
+
+		// assert for normal temperature - Email
+		assertTrue(TypewiseAlert.checkAndAlert(alertTarget, batteryCharacter, 20));
+
+		// assert alert for too low temperature - Controller
 		batteryCharacter = new BatteryCharacter();
 		batteryCharacter.setCoolingType(ECoolingType.PASSIVE_COOLING);
 		alertTarget = EAlertTarget.TO_CONTROLLER;
 		assertTrue(TypewiseAlert.checkAndAlert(alertTarget, batteryCharacter, -1));
-
-		batteryCharacter = new BatteryCharacter();
-		batteryCharacter.setCoolingType(ECoolingType.MED_ACTIVE_COOLING);
-		alertTarget = EAlertTarget.TO_EMAIL;
-		assertTrue(TypewiseAlert.checkAndAlert(alertTarget, batteryCharacter, -1));
-
-		batteryCharacter = new BatteryCharacter();
-		batteryCharacter.setCoolingType(ECoolingType.MED_ACTIVE_COOLING);
-		alertTarget = EAlertTarget.TO_EMAIL;
-		assertTrue(TypewiseAlert.checkAndAlert(alertTarget, batteryCharacter, 20));
 
 	}
 }
